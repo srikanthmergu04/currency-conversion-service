568dfb20396df8a6f01747db8e6cefe193d7b75e
Srikanth Mergu <srikanth.mergu@teradata.com>
2022-08-05 15:57:49 +0530

    invoking Currency Exchange Service from Currency Conversion Service using RESTTemplate.

--- src/main/java/com/java/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ src/main/java/com/java/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,10 +1,13 @@
 package com.java.microservices.currencyconversionservice.controller;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.java.microservices.currencyconversionservice.model.CurrencyConversion;
 
@@ -15,7 +18,20 @@ public class CurrencyConversionController {
 	public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to,
 			@PathVariable BigDecimal quantity) {
 
-		return new CurrencyConversion(10001L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+		HashMap<String, String> uriVariables = new HashMap<String, String>();
+
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+
+		ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity(
+				"http://localhost:8000/currency-exchange/{from}/to/{to}", CurrencyConversion.class, uriVariables);
+
+		CurrencyConversion currencyConversion = responseEntity.getBody();
+
+		currencyConversion.setQuantity(quantity);
+		currencyConversion.setTotalCalculatedAmount(quantity.multiply(currencyConversion.getConversionMultiple()));
+
+		return currencyConversion;
 
 	}
 
